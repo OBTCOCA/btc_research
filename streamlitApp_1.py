d4ca78466ddd07246c4dea89f773a387d6dad420
dataxad <88269206+dataxad@users.noreply.github.com>
2021-12-09 15:02:58 +0200

    commented out menu_items 'about'

--- streamlitApp_1.py
+++ streamlitApp_1.py
@@ -26,8 +26,8 @@ st.set_page_config(
      initial_sidebar_state="auto",
      menu_items={
         'Get Help': 'https://www.extremelycoolapp.com/help',
-         'Report a bug': "https://www.extremelycoolapp.com/bug",
-         'About': "# This is a header. This is an *extremely* cool app!"
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        #'About': "# This is a header. This is an *extremely* cool app!"
      }
  )
 
