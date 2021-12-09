389ce3f2359fcb624751de6f79c21bc574a72168
dataxad <88269206+dataxad@users.noreply.github.com>
2021-12-09 13:10:46 +0200

    fix indendations

--- streamlitApp_1.py
+++ streamlitApp_1.py
@@ -428,15 +428,13 @@ try:
             st.write('')
             #col1, col2, col3 = st.columns(3)
             #with col1:
-                st.metric(label='Accuracy', value=f'{prc}%')
+            st.metric(label='Accuracy', value=f'{prc}%')
             st.write('')
-
             #with col2:
-                st.metric(label="Today's Change Prediction", value=f'{pred}%')
+            st.metric(label="Today's Change Prediction", value=f'{pred}%')
             st.write('')
-
             #with col3:
-                st.metric(label="Today's Price Prediction", value=f'{price.iloc[-1]*(1+pred/100)}%')
+            st.metric(label="Today's Price Prediction", value=f'{price.iloc[-1]*(1+pred/100)}%')
             st.write('')
             st.write('#### Forecast evaluation by regressing "Target" on "Estimated"')
             X = sm.add_constant(Y['estimated'])
