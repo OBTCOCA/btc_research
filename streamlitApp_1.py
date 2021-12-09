7f9c5ae9ea0c72cc3c917e22960ce083e98259f9
dataxad <88269206+dataxad@users.noreply.github.com>
2021-12-09 14:01:56 +0200

    wrap st.metric in st.write

--- streamlitApp_1.py
+++ streamlitApp_1.py
@@ -433,7 +433,7 @@ try:
             #st.metric(label="Today's Change Prediction", value=f'{pred}%')
             st.write('')
             #with col3:
-            st.metric(label="Today's Price Prediction", value=f'{price.iloc[-1]*(1+pred/100)}%')
+            st.write(st.metric(label="Today's Price Prediction", value=str(f'{price.iloc[-1]*(1+pred/100)}%')))
             st.write('')
             st.write('#### Forecast evaluation by regressing "Target" on "Estimated"')
             X = sm.add_constant(Y['estimated'])
