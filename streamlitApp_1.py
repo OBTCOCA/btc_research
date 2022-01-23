77356a1d0456621172e8839ab3b691414c9cefce
dataxad <88269206+dataxad@users.noreply.github.com>
2022-01-23 14:44:56 +0200

    remove menu items

--- streamlitApp_1.py
+++ streamlitApp_1.py
@@ -25,10 +25,10 @@ st.set_page_config(
      page_icon="🤖",
      layout="wide",
      initial_sidebar_state="auto",
-     menu_items={
-        'Get Help': 'https://www.extremelycoolapp.com/help',
-        'Report a bug': "https://www.extremelycoolapp.com/bug",
-        'About': "# This is a header. This is an *extremely* cool app!"
+     #menu_items={
+     #   'Get Help': 'https://www.extremelycoolapp.com/help',
+     #   'Report a bug': "https://www.extremelycoolapp.com/bug",
+     #   'About': "# This is a header. This is an *extremely* cool app!"
      }
  )
 st.title('On-Chain Prediction Generator')
