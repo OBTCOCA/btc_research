225d839138cfbb1b9cea6d6817ffa530734fd4b8
dataxad <88269206+dataxad@users.noreply.github.com>
2022-01-23 14:42:58 +0200

    add menu items

--- streamlitApp_1.py
+++ streamlitApp_1.py
@@ -26,9 +26,9 @@ st.set_page_config(
      layout="wide",
      initial_sidebar_state="auto",
      menu_items={
-        #'Get Help': 'https://www.extremelycoolapp.com/help',
-        #'Report a bug': "https://www.extremelycoolapp.com/bug",
-        #'About': "# This is a header. This is an *extremely* cool app!"
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
      }
  )
 st.title('On-Chain Predidiction Generator')
